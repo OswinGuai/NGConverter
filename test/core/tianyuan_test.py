5eb38d3c14b408f919edbf64b11579d97f21fff5
OswinGuai <peizhyi@gmail.com>
2020-06-11 16:44:57 +0800

    Test instrument.

--- test/core/tianyuan_test.py
+++ test/core/tianyuan_test.py
@@ -7,6 +7,10 @@ from ngconverter.core.finetune import FineTuneAPI
 from ngconverter.core.convert import ConvertAPI
 import embedded_model.object_detection.utils.config_util as config_util
 
+"""
+    Preliminary: You need to download test/resources from the repository to run this test.
+
+"""
 class TestTianyuanProcess(unittest.TestCase):
 
     finetuner = FineTuneAPI()
@@ -45,17 +49,17 @@ class TestTianyuanProcess(unittest.TestCase):
         pipeline_config_path = os.path.join(target_dir, "pipeline.config")
         train_dir = os.path.join(target_dir, "trained_models")
         train_steps = 2
-
-        # configs = config_util.get_configs_from_pipeline_file(ori_pipeline_config_path, config_override=input_config_list)
-        #
-        # pipeline_proto = config_util.create_pipeline_proto_from_configs(configs)
-        # os.remove(pipeline_config_path)
-        # config_util.save_pipeline_config(pipeline_proto, target_dir)
-        # shutil.rmtree(train_dir)
-        # self.finetuner.finetune_embedded_objectdetection_model(pipeline_config_path, train_dir, train_steps=train_steps)
-        # print("finetune is finished.")
-
         model_path = os.path.join(train_dir, "model.ckpt-%d" % train_steps)
+
+        configs = config_util.get_configs_from_pipeline_file(ori_pipeline_config_path, config_override=input_config_list)
+        pipeline_proto = config_util.create_pipeline_proto_from_configs(configs)
+        os.remove(pipeline_config_path)
+        config_util.save_pipeline_config(pipeline_proto, target_dir)
+        shutil.rmtree(train_dir)
+
+        self.finetuner.finetune_embedded_objectdetection_model(pipeline_config_path, train_dir, train_steps=train_steps)
+        print("finetune is finished.")
+
         self.converter.convert_objectdetection_tf1(pipeline_config_path, model_path, target_dir)
         print("convert is finished.")
 
