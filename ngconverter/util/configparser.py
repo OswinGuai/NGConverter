389eb89146193101b9f8f94fa627f34c393c8fec
OswinGuai <peizhyi@gmail.com>
2020-07-01 12:50:39 +0800

    Modify converting.

--- ngconverter/util/configparser.py
+++ ngconverter/util/configparser.py
@@ -11,31 +11,32 @@ def load_config(file_path):
     return config_info
 
 def instance_tf_objectdetection_model_config(embedded_model_config, target_dir, train_dataset_path, eval_dataset_path, label_path):
-    # update_config = {
-    #     "train_dataset_path": train_dataset_path,
-    #     "eval_dataset_path": eval_dataset_path,
-    #     "label_path": label_path
-    # }
-    # input_config_list = [
-    #     """
-    #     train_input_reader: {{
-    #         tf_record_input_reader {{
-    #           input_path: "{train_dataset_path}"
-    #         }}
-    #         label_map_path: "{label_path}"
-    #       }}
-    #     """.format(**update_config),
-    #     """
-    #       eval_input_reader: {{
-    #         tf_record_input_reader {{
-    #           input_path: "{eval_dataset_path}"
-    #         }}
-    #         label_map_path: "{label_path}"
-    #         shuffle: false
-    #         num_readers: 1
-    #       }}
-    #     """.format(**update_config)
-    # ]
+    update_config = {
+        "train_dataset_path": train_dataset_path,
+        "eval_dataset_path": eval_dataset_path,
+        "label_path": label_path,
+        "fine_tune_checkpoint": "embedded_model/pretrained/ssd_mobilenet_v1_0.75_depth_300x300_coco14_sync_2018_07_03/model.ckpt"
+    }
+    input_config_list = [
+        """
+        train_input_reader: {{
+            tf_record_input_reader {{
+              input_path: "{train_dataset_path}"
+            }}
+            label_map_path: "{label_path}"
+          }}
+        """.format(**update_config),
+        """
+          eval_input_reader: {{
+            tf_record_input_reader {{
+              input_path: "{eval_dataset_path}"
+            }}
+            label_map_path: "{label_path}"
+            shuffle: false
+            num_readers: 1
+          }}
+        """.format(**update_config)
+    ]
     pipeline_config_path = os.path.join(target_dir, "pipeline.config")
     if os.path.exists(pipeline_config_path):
         os.remove(pipeline_config_path)
