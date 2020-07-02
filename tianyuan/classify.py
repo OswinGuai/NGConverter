3bd9d2ca773b4241bd8d44b08a8c2936a5c196e4
OswinGuai <peizhyi@gmail.com>
2020-07-02 18:34:23 +0800

    update test files

--- tianyuan/classify.py
+++ tianyuan/classify.py
@@ -2,6 +2,7 @@ import unittest
 
 from ngconverter.core.task import Task
 from ngconverter.util.configparser import load_config
+import time
 import shutil
 
 """
@@ -15,17 +16,13 @@ class TestTask(unittest.TestCase):
         pass
 
     def test_build_from_config(self):
-        self.task_name = "task_ty_classify_0702"
-        config_path = "test/resources/train_classification_demo_config.yaml"
-        #self.task_name = "task_ty_detect_0702"
-        #config_path = "test/resources/test_ty_detect_config.yaml"
+        timestamp = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+        self.task_name = "task_ty_classify_%s" % timestamp
+        config_path = "tianyuan/ty_classification_config.yaml"
         config = load_config(config_path)
         task = Task.Builder.init_by_config(self.task_name, config)
         task.execute()
         task.hold()
-
-        self.assertEqual('foo'.upper(), 'FOO')
-
 
 if __name__ == '__main__':
     unittest.main()
