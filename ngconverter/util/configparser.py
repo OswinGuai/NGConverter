8c8c2a805cfc90e41c98b3fe172dd6466607edd8
OswinGuai <peizhyi@gmail.com>
2020-07-11 11:34:02 +0800

    deploy model.

--- ngconverter/util/configparser.py
+++ ngconverter/util/configparser.py
@@ -2,10 +2,13 @@ import yaml
 import os
 from ngconverter.core.configuration import ConfigInfo
 from embedded_model.object_detection.utils import label_map_util
+from urllib import request
+import shutil
+import sys
 
 _EMBEDDED_MODEL_CHECKPOINT = "~/.nglite/pretrained"
 _EMBEDDED_OBJECTDETECTION_NAME = "ssd_mobilenet_v1_0.75_depth_300x300_coco14_sync_2018_07_03"
-
+_EMBEDDED_OBJECTDETECTION_URL = "http://download.tensorflow.org/models/object_detection/ssd_mobilenet_v1_0.75_depth_300x300_coco14_sync_2018_07_03.tar.gz"
 def load_config(file_path):
     f = open(file_path)
     yaml_config = yaml.load(f, Loader=yaml.FullLoader)
@@ -23,6 +26,15 @@ def instance_embedded_tf_objectdetection_model_config(
     label_dict = label_map_util.get_label_map_dict(label_path, False)
     num_class = len(label_dict.keys())
     pretrained_model_dir = os.path.join(_EMBEDDED_MODEL_CHECKPOINT, _EMBEDDED_OBJECTDETECTION_NAME)
+    if not os.path.exists(pretrained_model_dir):# Download and extract the model from the internet.
+        sys.stdout.write("Pretrained model has not been deployed. Do it now...\n")
+        local_pack = os.path.join("~/.nglite", _EMBEDDED_OBJECTDETECTION_NAME + ".tar.gz")
+        sys.stdout.write("Download pretrained model from %s to %s...\n" % (_EMBEDDED_OBJECTDETECTION_URL, local_pack))
+        sys.stdout.flush()
+        request.urlretrieve(_EMBEDDED_OBJECTDETECTION_URL, local_pack)
+        sys.stdout.write("Extract pretrained model to %s...\n" % pretrained_model_dir)
+        sys.stdout.flush()
+        shutil.unpack_archive(local_pack, pretrained_model_dir)
     pretrained_model = os.path.join(pretrained_model_dir, "model.ckpt")
     update_config = {
         "num_classes": num_class,
